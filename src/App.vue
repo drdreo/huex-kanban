3d16cbfb5f97a92a58ba10458892c577ddd8e1d0
David Holzmann <david.holzmann@storyclash.com>
2019-03-09 22:22:20 +0100

    created task component

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,19 @@
 <template>
     <div id="app">
         <img alt="Vue logo" src="./assets/logo.png">
-        <HelloWorld msg="Welcome to Your Vue.js App"/>
+        <Task title="Auto waschen" description="Das Auto ist dreckig und muss gewaschen werden!" active="false"></Task>
     </div>
 </template>
 
 <script>
     import HelloWorld from './components/HelloWorld.vue';
+    import Task from './components/task/Task';
 
     export default {
         name: 'app',
         components: {
-            HelloWorld
+            HelloWorld,
+            Task
         }
     };
 </script>
---
+++ src/components/task/Task.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="task">
+        <h2>{{ title }}</h2>
+        <p>{{description}}</p>
+        <input type="checkbox" v-model="active">
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'Task',
+        props: {
+            title: String,
+            description: String,
+            active: Boolean,
+        }
+    };
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+    @import "Task.scss";
+</style>
