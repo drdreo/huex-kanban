19b3a9ec030d623dcd8b9b9c807f01bd7f95c491
David <david.holzmann95@gmail.com>
2019-03-31 21:28:20 +0200

    created view and components

---
+++ src/components/Column/Column.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="board-column">
+
+    </div>
+</template>
+
+<script lang="ts">
+    import {Component, Prop, Vue} from "vue-property-decorator";
+
+    @Component
+    export default class Column extends Vue {
+
+    }
+</script>
---
+++ src/pages/TasksView/TasksView.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="tasks-view">
+        <p>hallo dasgs fju</p>
+    </div>
+</template>
+
+<script lang="ts">
+    import {State, Action} from "vuex-class";
+    import {Component, Vue} from "vue-property-decorator";
+</script>
+
+<style lang="scss" scoped>
+    @import "TasksView.scss";
+</style>
